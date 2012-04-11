4c123fd412957ffe1842dfcc78051f2adb85b419
Wolmir Nemitz <neocorrupt@gmail.com>
2012-04-11 15:33:34 -0300

    Stuff.

--- rpv/teste/views.py
+++ rpv/teste/views.py
@@ -1,4 +1,4 @@
 # Create your views here.
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse('Ola, este e um teste do django')
+    return HttpResponse('Ola, este é um teste do django')
