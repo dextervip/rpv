161c9ad91ac9adbb9247df8e8580bc517e05c04e
Rafael <jrtadf@gmail.com>
2012-04-11 15:01:38 -0300

    Texto home

--- rpv/teste/views.py
+++ rpv/teste/views.py
@@ -1,4 +1,4 @@
 # Create your views here.
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse('Teste do Django')
+    return HttpResponse('Olá, este é um teste do django')
