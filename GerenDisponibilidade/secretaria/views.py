bd114da5ba8cea77753417454a4a830135602bea
Rafael <jrtadf@gmail.com>
2012-05-05 02:00:14 -0300

    Iniciado modulo de secretaria

--- GerenDisponibilidade/secretaria/views.py
+++ GerenDisponibilidade/secretaria/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+# -*- encoding: utf-8 -*-
+
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+
+
+def home(request):
+    context_instance = RequestContext(request)
+    return render_to_response("secretaria/home.html", context_instance)
