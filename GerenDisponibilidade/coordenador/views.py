4d39672e2e56c5dbc7b586b90266d1624d499acb
Juliano Rodovalho <j.rodovalho.m@gmail.com>
2012-05-19 08:53:10 -0300

    Refatorações na app coordenador. ref #314

--- GerenDisponibilidade/coordenador/views.py
+++ GerenDisponibilidade/coordenador/views.py
@@ -1,3 +1,9 @@
 # -*- encoding: utf-8 -*-
 
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def home(request):
+    context = RequestContext(request)
+    return render_to_response("coordenador/home.html", context)
 
