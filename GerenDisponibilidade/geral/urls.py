6af1949af9db45c49c55c1e2b96e0d9dfb27e388
Juliano Rodovalho <j.rodovalho.m@gmail.com>
2012-09-19 09:20:04 -0300

    Alterações no redirecionamneto de URLs (Login-Geral-Home)

--- GerenDisponibilidade/geral/urls.py
+++ GerenDisponibilidade/geral/urls.py
@@ -1,1 +1,7 @@
 from django.conf.urls.defaults import patterns, url, include
+
+urlpatterns = patterns('geral.views',
+    url(r'^$', 'pageLogin', name='pageLogin'),
+    url(r'^paginaInicial$', 'home', name='home'),
+    
+)
