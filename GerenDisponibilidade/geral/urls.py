31a611080b0e73320fad99f0cb637c2975fb1094
Juliano Rodovalho <j.rodovalho.m@gmail.com>
2012-10-03 08:38:32 -0300

    [geral--url-patterns--registration] Melhorias nas rotas-URLS

--- GerenDisponibilidade/geral/urls.py
+++ GerenDisponibilidade/geral/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import patterns, url, include
 
 urlpatterns = patterns('geral.views',
-    url(r'^$', 'pageLogin', name='pageLogin'),
-    url(r'^paginaInicial$', 'home', name='home'),
+    url(r'^$', 'home', name='home'),
+    #url(r'^pageLogin$', 'pageLogin', name='pageLogin'),
+    url(r'^sobre$', 'sobre', name= 'sobre'),
+    url(r'^contato$', 'contato', name= 'contato'),
     
 )
