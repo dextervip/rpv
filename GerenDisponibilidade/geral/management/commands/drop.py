0a9ebb7bad27cdbe9c1aa30c40172df5b1be0db6
Rafael <jrtadf@gmail.com>
2012-10-03 14:19:03 -0300

    Correção do metodo drop

--- GerenDisponibilidade/geral/management/commands/drop.py
+++ GerenDisponibilidade/geral/management/commands/drop.py
@@ -17,11 +17,11 @@ class Command(NoArgsCommand):
             print "Connecting..."
             db=MySQLdb.connect(host=settings.DATABASES['default']['HOST'] or "localhost" ,user=settings.DATABASES['default']['USER'],
                                passwd=settings.DATABASES['default']['PASSWORD'], port=int(settings.DATABASES['default']['PORT'] or 3306))
-        except Exception as error:
-            print "An Error Occurred: ", error
+
             cursor = db.cursor()
             print "Dropping database %s" % settings.DATABASES['default']['NAME']
             cursor.execute("drop database %s; create database %s;" % (settings.DATABASES['default']['NAME'], settings.DATABASES['default']['NAME']))
             print "Dropped"
-        except Exception as error2:
-            print "An Error Occurred: ", error2
+            
+        except Exception as error:
+            print "An Error Occurred: ", error
