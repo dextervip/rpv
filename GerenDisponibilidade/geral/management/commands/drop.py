a8d63265ddfc8dba326196faadb84ae3130b1af1
Juliano Rodovalho <j.rodovalho.m@gmail.com>
2012-10-03 11:12:07 -0300

    [drop.py] Add Try and Except in code

--- GerenDisponibilidade/geral/management/commands/drop.py
+++ GerenDisponibilidade/geral/management/commands/drop.py
@@ -12,12 +12,16 @@ class Command(NoArgsCommand):
     def handle_noargs(self, **options):
 
         import MySQLdb
-
-        print "Connecting..."
-        db=MySQLdb.connect(host=settings.DATABASES['default']['HOST'] or "localhost" ,user=settings.DATABASES['default']['USER'],
-        passwd=settings.DATABASES['default']['PASSWORD'], port=int(settings.DATABASES['default']['PORT'] or 3306))
-
-        cursor = db.cursor()
-        print "Dropping database %s" % settings.DATABASES['default']['NAME']
-        cursor.execute("drop database %s; create database %s;" % (settings.DATABASES['default']['NAME'], settings.DATABASES['default']['NAME']))
-        print "Dropped"
+        
+        try:
+            print "Connecting..."
+            db=MySQLdb.connect(host=settings.DATABASES['default']['HOST'] or "localhost" ,user=settings.DATABASES['default']['USER'],
+                               passwd=settings.DATABASES['default']['PASSWORD'], port=int(settings.DATABASES['default']['PORT'] or 3306))
+        except Exception as error:
+            print "An Error Occurred: ", error
+            cursor = db.cursor()
+            print "Dropping database %s" % settings.DATABASES['default']['NAME']
+            cursor.execute("drop database %s; create database %s;" % (settings.DATABASES['default']['NAME'], settings.DATABASES['default']['NAME']))
+            print "Dropped"
+        except Exception as error2:
+            print "An Error Occurred: ", error2
